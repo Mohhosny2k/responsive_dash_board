2fe80755ba79959a7ba368f5578f3ed42c37be54
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2024-04-21 13:05:44 +0200

    create dashboard desktop layout

---
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashboradDeskTopLayout extends StatelessWidget {
+  const DashboradDeskTopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
