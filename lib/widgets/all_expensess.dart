6886508fada6658569217f0e590994ff50bbe577
Mohamed Hosny <92595555+Mohhosny2k@users.noreply.github.com>
2024-05-12 15:22:13 +0300

    all expenses header part 1

--- lib/widgets/all_expensess.dart
+++ lib/widgets/all_expensess.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/all_expensess_header.dart';
 
 class AllExpensess extends StatelessWidget {
   const AllExpensess({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding:const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child:const Column(
+        children: [
+          AllExpensessHeader()  ,
+        ]
+    ),
+    );
   }
 }
+
