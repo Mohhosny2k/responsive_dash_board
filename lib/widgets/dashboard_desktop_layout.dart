9da52480978c65a17f92a1784e37301174deb101
Mohamed Hosny <92595555+Mohhosny2k@users.noreply.github.com>
2024-05-12 15:12:49 +0300

    create all expenses widget

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:responsive_dash_board/widgets/all_expensess.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 
 class DashboradDeskTopLayout extends StatelessWidget {
@@ -7,9 +9,22 @@ class DashboradDeskTopLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return const Row(
       children: [
         Expanded(child: CustomDrawer()),
+        SizedBox(
+          width: 32,
+        ),
+        Expanded(
+          flex: 2,
+            child: Column(
+          children: [
+            Expanded(child: AllExpensess()),
+          ],
+        )),
+        SizedBox(
+          width: 32,
+        ),
       ],
     );
   }
