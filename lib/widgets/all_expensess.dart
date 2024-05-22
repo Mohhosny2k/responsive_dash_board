ea1391142fff6d139a98913bda16b3e3a2f4ad53
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2024-05-22 15:26:46 +0300

    create custom background widget

--- lib/widgets/all_expensess.dart
+++ lib/widgets/all_expensess.dart
@@ -2,30 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expensess_header.dart';
 import 'package:responsive_dash_board/widgets/all_expensess_items_list_view.dart';
 
-
+import 'custom_background_container.dart';
 
 class AllExpensess extends StatelessWidget {
   const AllExpensess({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding:const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child:const Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          AllExpensessHeader(),
-          SizedBox(height: 16,),
-          // AllExpensessItem(itemModel: AllExpensessItemModel(image: Assets.imagesIncome, title: 'Income', date: 'April 2022', price: r'$ 20.129'))
-         AllExpensessItemsListView(),
-       
-        ]
-    ),
+    return const CustomBackgroundContainer(
+      child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+        AllExpensessHeader(),
+        SizedBox(
+          height: 16,
+        ),
+        // AllExpensessItem(itemModel: AllExpensessItemModel(image: Assets.imagesIncome, title: 'Income', date: 'April 2022', price: r'$ 20.129'))
+        AllExpensessItemsListView(),
+      ]),
+    //  padding: 20.0,
     );
   }
 }
-
