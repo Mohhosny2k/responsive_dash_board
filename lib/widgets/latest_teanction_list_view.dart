934b8e22eeff6fd43f32b218a1c2353cb04f4e28
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2024-07-14 13:59:50 +0300

    intrinsic width /\/\/\

--- lib/widgets/latest_teanction_list_view.dart
+++ lib/widgets/latest_teanction_list_view.dart
@@ -7,21 +7,29 @@ import '../utils/app_images.dart';
 class LatestTeansctionListView extends StatelessWidget {
   const LatestTeansctionListView({super.key});
   static const items = [
-    UserInfoModel(image: Assets.imagesAvatar1, title: 'Madrani Andi', subTitle: 'Madraniandi20@gmail.com'),
-    UserInfoModel(image: Assets.imagesAvatar2, title: 'Madrani Andi', subTitle: 'Madraniandi20@gmail.com'),
-    UserInfoModel(image: Assets.imagesAvatar3, title: 'Madrani Andi', subTitle: 'Madraniandi20@gmail.com'),
+    UserInfoModel(
+        image: Assets.imagesAvatar1,
+        title: 'Madrani Andi',
+        subTitle: 'Madraniandi20@gmail.com'),
+    UserInfoModel(
+        image: Assets.imagesAvatar2,
+        title: 'Madrani Andi',
+        subTitle: 'Madraniandi20@gmail.com'),
+    UserInfoModel(
+        image: Assets.imagesAvatar3,
+        title: 'Madrani Andi',
+        subTitle: 'Madraniandi20@gmail.com'),
   ];
   @override
   Widget build(BuildContext context) {
-    return ListView.builder
-    (
+    return ListView.builder(
       scrollDirection: Axis.horizontal,
       itemCount: items.length,
       itemBuilder: (context, index) {
         return Padding(
-          padding: const EdgeInsets.only(top: 20),
-          child: UserInfoListTile(userInfoModel: items[index])
-        );
+            padding: const EdgeInsets.only(top: 20),
+            child: IntrinsicWidth(
+                child: UserInfoListTile(userInfoModel: items[index])));
       },
     );
   }
