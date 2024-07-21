d8fcc43c85e15437c5d8af6c49b9ef1884c52dcf
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2024-07-21 14:40:47 +0300

    custom button

--- lib/widgets/quick_invoice_form.dart
+++ lib/widgets/quick_invoice_form.dart
@@ -2,26 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:responsive_dash_board/widgets/title_text_field.dart';
 
+import 'custom_button.dart';
+
 class QuickInvoiceForm extends StatelessWidget {
   const QuickInvoiceForm({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const  Column(
+    return const Column(
       children: [
-        Row(children:  [
-
-          Expanded(child: TitleTextField  (title: 'Customer name', hint:'Type customer name' ,)),
-         SizedBox(width: 16,),
-          Expanded(child: TitleTextField  (title: 'Customer Email', hint:'Type customer email' ,)),
+        Row(children: [
+          Expanded(
+              child: TitleTextField(
+            title: 'Customer name',
+            hint: 'Type customer name',
+          )),
+          SizedBox(
+            width: 16,
+          ),
+          Expanded(
+              child: TitleTextField(
+            title: 'Customer Email',
+            hint: 'Type customer email',
+          )),
         ]),
-        SizedBox(height: 24,),
-         Row(children:  [
-
-          Expanded(child: TitleTextField  (title: 'Item name', hint:'Type customer name' ,)),
-         SizedBox(width: 16,),
-          Expanded(child: TitleTextField  (title: ' Item mount', hint:'USD' ,)),
+        SizedBox(
+          height: 24,
+        ),
+        Row(children: [
+          Expanded(
+              child: TitleTextField(
+            title: 'Item name',
+            hint: 'Type customer name',
+          )),
+          SizedBox(
+            width: 16,
+          ),
+          Expanded(
+              child: TitleTextField(
+            title: ' Item mount',
+            hint: 'USD',
+          )),
         ]),
+        Row(
+          children: [
+            Expanded(child: CustomButton()),
+          ],
+        ),
       ],
     );
   }
