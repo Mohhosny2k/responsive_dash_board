325bf8d7cff67e5e7332b86e2890cdf468f0834a
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2024-07-23 15:31:16 +0300

    my cards page view part two

--- lib/widgets/my_cards_page_view.dart
+++ lib/widgets/my_cards_page_view.dart
@@ -1,20 +1,27 @@
+import 'package:expandable_page_view/expandable_page_view.dart';
 import 'package:flutter/material.dart';
 
 import 'my_card.dart';
 
 class MyCardsPageView extends StatelessWidget {
   const MyCardsPageView({super.key});
-  List<double> childerenHeight = [];
-  int currentPage = 0;
+  // List<double> childerenHeight = [];
+  // int currentPage = 0;
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: childerenHeight[currentPage],
-      child: PageView.builder(
-        itemCount: 3,
+    // return SizedBox(
+    //  // height: childerenHeight[currentPage],
+    //   child: PageView.builder(
+    //     itemCount: 3,
+    //     scrollDirection: Axis.horizontal,
+    //     itemBuilder: (context, index) => const MyCard(),
+    //   ),
+    // );
+    return ExpandablePageView(
         scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) => const MyCard(),
-      ),
-    );
+        children: List.generate(
+          3,
+          (index) => const MyCard(),
+        ));
   }
 }
