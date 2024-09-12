8ffa5ebe2789e0797cb3126c4f19b46a983f167b
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2024-09-12 17:28:56 +0300

    tablet layout paer two 115

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
 import 'package:responsive_dash_board/widgets/dashboard_tablet_layout.dart';
 import '../widgets/dashboard_desktop_layout.dart';
+import '../widgets/dashboard_mobile_layout.dart';
 import '../widgets/my_cards_section.dart';
 
 class DashBoradView extends StatelessWidget {
@@ -12,7 +13,7 @@ class DashBoradView extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFFF7F9FA),
       body: AdaptiveLayout(
-          mobileLayout: (context) =>const SizedBox(),
+          mobileLayout: (context) =>const  DashBoardMobileLayout(),
           tabletLayout: (context) =>const DashBoardTabletLayout(),
           desktopLayout: (context) =>const DashboradDeskTopLayout()),
     );
--- lib/widgets/dashboard_tablet_layout.dart
+++ lib/widgets/dashboard_tablet_layout.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expensess_and_quick_invoice.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/income_section.dart';
+import 'package:responsive_dash_board/widgets/my_cards_and_transaction_history_section.dart';
+
+import 'dashboard_mobile_layout.dart';
 
 class DashBoardTabletLayout extends StatelessWidget {
   const DashBoardTabletLayout({super.key});
@@ -8,13 +12,18 @@ class DashBoardTabletLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Row(
-      children: [Expanded(child: CustomDrawer()),
-      SizedBox(width: 32,),
-       Expanded(
-        flex: 3,
-        child: AllExpensessAndQuickInvoiceSection()),
-        SizedBox(width: 32,),
-        ],
+      children: [
+        Expanded(child: CustomDrawer()),
+        SizedBox(
+          width: 32,
+        ),
+        Expanded(
+            flex: 3,
+            child: DashBoardMobileLayout()),
+        SizedBox(
+          width: 32,
+        ),
+      ],
     );
   }
 }
