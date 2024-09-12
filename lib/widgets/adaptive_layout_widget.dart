361c1c57e8a0b6e7606874a402c57f97bcd930a3
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2024-09-12 16:02:39 +0300

    tablet layout part 1

--- lib/widgets/adaptive_layout_widget.dart
+++ lib/widgets/adaptive_layout_widget.dart
@@ -7,9 +7,9 @@ final WidgetBuilder mobileLayout,tabletLayout,desktopLayout;
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
      //   print('layout builder width =${constraints.maxWidth}');
-        if (constraints.maxWidth < 600) {
+        if (constraints.maxWidth < 800) {
           return  mobileLayout(context);
-        }  else if(constraints.maxWidth < 900){
+        }  else if(constraints.maxWidth < 1200){
           return  tabletLayout(context);
         } else {
           return  desktopLayout(context);
