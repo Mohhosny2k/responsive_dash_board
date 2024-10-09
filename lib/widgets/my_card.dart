c029972bc4dc6989753bdcb74cc5f02aaa05b58b
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2024-10-09 17:07:47 +0300

    responsive desktop layout done

--- lib/widgets/my_card.dart
+++ lib/widgets/my_card.dart
@@ -55,10 +55,12 @@ class MyCard extends StatelessWidget {
                 ],
               ),
             ),
-            const SizedBox(
-              height: 54 - 28,
+          const  Flexible(
+              child:  SizedBox(
+                height: 54 - 28,
+              ),
             ),
-          ],
+          ], 
         ),
       ),
     );
